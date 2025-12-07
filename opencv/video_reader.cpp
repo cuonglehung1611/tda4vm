7bca405b6a0c178230b20602826060faa0bed7b9
cuonglehung1611 <cuonglehung1611@gmail.com>
2025-12-07 10:40:02 +0700

    docker is working cam_capture.cpp is working

--- opencv/video_reader.cpp
+++ opencv/video_reader.cpp
@@ -3,14 +3,14 @@
 
 int main()
 {
-	std::string filename = "video.avi";
-	cv::VideoCapture cap(filename);
+    std::string filename = "video.avi";
+    cv::VideoCapture cap(filename);
 
-	if (!cap.isOpened()) {
+    if (!cap.isOpened()) {
         std::cerr << "Error: Cannot open video file '" << filename << "'\n";
         std::cerr << "Make sure the file exists and the path is correct.\n";
         return -1;
     }
-	return 0;
+    return 0;
 
 }
